fe80da367c048db085e2c167047f5f6dc48b2217
liti1921 <56065039+liti1921@users.noreply.github.com>
2020-05-03 14:05:16 +0800

    电影详情
    moviedetail

---
+++ src/main/java/com/example/recommend/controller/MoviedetailController.java
@@ -0,0 +1,55 @@
+package com.example.recommend.controller;
+
+import com.example.recommend.dao.MoviedetailDao;
+import com.example.recommend.entity.Movies;
+import com.example.recommend.entity.ResponseBean;
+import com.example.recommend.service.AdminMovieService;
+import com.example.recommend.service.MoviedetailService;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("//Moviedetail")
+public class MoviedetailController {
+
+    @Autowired
+    private MoviedetailService moviedetailService;
+
+    @GetMapping("//getMoviedetail")
+    public @ResponseBody
+    ResponseBean Getmovie(@RequestParam long movieId){
+        /*JSONObject json = JSONObject.fromObject(movieId);
+        long movieId1 = json.getLong("movieId");*/
+        ResponseBean response = new ResponseBean();
+        try{
+            List<Movies> moviesList = moviedetailService.Getmovie(movieId);
+            response.setSuccess(true);
+            response.setData(moviesList);
+        }
+        catch (Exception e){
+            response.setSuccess(false);
+            response.setErrMsg(e.getMessage());
+        }
+        return response;
+    }
+
+    @GetMapping("/selectAllMovies")
+    public @ResponseBody
+    ResponseBean selectAllMovies(){
+        ResponseBean response = new ResponseBean();
+        try{
+            List<Movies> moviesList = moviedetailService.getAllMovieList();
+            response.setSuccess(true);
+            response.setData(moviesList);
+        }
+        catch (Exception e){
+            response.setSuccess(false);
+            response.setErrMsg(e.getMessage());
+        }
+        return response;
+    }
+
+}
