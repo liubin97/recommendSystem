90000e2d7573f8ed8273a43b9899fb05b0bb0847
tao tang <835905289@qq.com>
2020-05-11 19:50:38 +0800

    moviehomeimple

---
+++ src/main/java/com/example/recommend/service/impl/MoviehomeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.recommend.service.impl;
+
+import com.example.recommend.entity.Movies;
+import com.example.recommend.service.MoviehomeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MoviehomeServiceImpl implements MoviehomeService {
+    @Override
+    public List<Movies> getAllMovieList() {
+        return null;
+    }
+
+    @Override
+    public List<Movies> selectmovie(long Title) {
+        return null;
+    }
+}
