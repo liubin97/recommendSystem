2254cc8ee470cfe99132475b48f07aaa3589c9ff
liubint <liubint@yonyou.com>
2020-06-28 22:38:18 +0800

    错误修改

--- src/main/java/com/example/recommend/service/impl/MoviehomeServiceImpl.java
+++ src/main/java/com/example/recommend/service/impl/MoviehomeServiceImpl.java
@@ -1,24 +1,24 @@
 package com.example.recommend.service.impl;
 
-import com.example.recommend.dao.MoviehomeDao;
 import com.example.recommend.entity.Movies;
 import com.example.recommend.service.MoviehomeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
-public class MoviehomeServiceimpl implements MoviehomeService {
-    @Autowired
-    MoviehomeDao moviehomeDao;
+class MoviehomeServiceImpl implements MoviehomeService {
     @Override
     public List<Movies> getAllMovieList() {
-        return moviehomeDao.getAllMovieList();
+        return null;
+    }
+
+    @Override
+    public List<Movies> selectmovie(long Title) {
+        return null;
     }
 
     @Override
     public Movies selectmovie(String Title) {
-        return moviehomeDao.selectmovie(Title);
+        return null;
     }
 }
--- src/main/java/com/example/recommend/service/impl/MoviehomeServiceimpl.java
+++ src/main/java/com/example/recommend/service/impl/MoviehomeServiceimpl.java
@@ -1,15 +1,12 @@
 package com.example.recommend.service.impl;
 
-import com.example.recommend.dao.MoviehomeDao;
 import com.example.recommend.entity.Movies;
 import com.example.recommend.service.MoviehomeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
-class MoviehomeServiceimpl implements MoviehomeService {
+class MoviehomeServiceImpl implements MoviehomeService {
     @Override
     public List<Movies> getAllMovieList() {
         return null;
@@ -24,5 +21,4 @@ class MoviehomeServiceimpl implements MoviehomeService {
     public Movies selectmovie(String Title) {
         return null;
     }
-
 }
